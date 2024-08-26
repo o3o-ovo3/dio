75b836b235df9006a501aad90859ccc46d80dc20
Yunju Jang <rlo3olr@naver.com>
2024-08-26 17:45:41 +0900

    vue project 생성

--- src/App.vue
+++ src/App.vue
@@ -107,32 +107,3 @@ export default {
     }*/
   }
 </script>
-<!--
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Hello Vue World"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
-
--->
